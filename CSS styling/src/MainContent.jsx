256b2ec9fcfa0b7b739511a35c56c1180f48cdd2
Narayanadhikari9 <Narayanadhikari9@gmail.com>
2023-09-07 13:49:04 -0500

    layout styling css

--- CSS styling/src/MainContent.jsx
+++
@@ -1,4 +0,0 @@
-function MainContent() {
-  return <mainContent className="mainContent">I am main Content</mainContent>;
-}
-export default MainContent;
---
+++ CSS styling/src/components/mainContent.jsx
@@ -0,0 +1,5 @@
+import styles from "./mainContent.module.css";
+function MainContent() {
+  return <mainContent className="mainContent">Restaurant</mainContent>
+}
+export default MainContent;
