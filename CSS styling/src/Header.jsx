d7e3c84af0b6af811621ead019270a4cefd07df5
Narayanadhikari9 <Narayanadhikari9@gmail.com>
2023-09-06 23:28:52 -0500

    add compountant

---
+++ CSS styling/src/Header.jsx
@@ -0,0 +1,10 @@
+import "./Header.css";
+function Header() {
+  return (
+    <header className="header">
+      <img src="https://static.toiimg.com/photo/msid-87867224,width-96,height-65.cms" />
+      <h1>This is beautiful place</h1>
+    </header>
+  );
+}
+export default Header;
---
+++ CSS styling/src/MainContent.jsx
@@ -0,0 +1,4 @@
+function MainContent() {
+  return <mainContent className="mainContent">I am main Content</mainContent>;
+}
+export default MainContent;
